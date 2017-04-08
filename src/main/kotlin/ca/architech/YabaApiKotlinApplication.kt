fd3c94f0f8b91caa847f9ece7d632bbf2a86c6e2
Jungho <jk@architech.ca>
2017-04-08 11:44:28 -0400

    removed JPA dependency and specified collection for the Post entity

--- src/main/kotlin/ca/architech/YabaApiKotlinApplication.kt
+++ src/main/kotlin/ca/architech/YabaApiKotlinApplication.kt
@@ -2,10 +2,8 @@ package ca.architech
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.context.properties.EnableConfigurationProperties
 
 @SpringBootApplication
-@EnableConfigurationProperties
 class YabaApiKotlinApplication
 
 fun main(args: Array<String>) {
--- src/main/kotlin/ca/architech/api/Post.kt
+++ src/main/kotlin/ca/architech/api/Post.kt
@@ -1,5 +1,7 @@
 package ca.architech.api
 
+import org.springframework.data.mongodb.core.mapping.Document
 import java.io.Serializable
 
+@Document(collection = "posts")
 class Post (val id: String, val categories: String, val content: String) : Serializable
